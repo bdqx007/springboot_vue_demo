d388cd0fe539e963b8465e6b7a9c71bd649fe839
bdqx_007@163.com <bdqx_007@163.com>
2019-12-02 09:06:24 +0800

    创建前后端分离实现登录项目

---
+++ 前后端分离实现登录/server/src/main/java/com/wg/SpringbootVueLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueLoginDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueLoginDemoApplication.class, args);
+    }
+
+}
---
+++ 前后端分离实现登录/server/src/test/java/com/wg/SpringbootVueLoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueLoginDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
